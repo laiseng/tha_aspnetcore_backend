5cf1565370a038dc85e5a5a22e421f4c308f0926
Lai Seng <glaiseng@gmail.com>
2021-05-04 12:50:48 +0800

    chore: scaffolding aspnetcore5 project

---
+++ Models/THA.Modal.User/IUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace THA.Modal.User
+{
+   public class IUser
+   {
+      public int ID { get; set; }
+      public int FIRST_NAME { get; set; }
+      public int LAST_NAME { get; set; }
+      public int EMAIL { get; set; }
+      public int ACCESS_RIGHTS { get; set; }
+
+   }
+}
